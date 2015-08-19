b6c3c1c424f1b3325b0a6c30473c9080ad95032d
Nacho Salvador <nachosalvador83@gmail.com>
2015-08-19 15:36:18 +0200

    Added players to TennisGame

--- src/TennisGame/TennisGame.php
+++ src/TennisGame/TennisGame.php
@@ -3,5 +3,15 @@
 namespace TennisGame;
 
 class TennisGame {
-	
+	private $player1;
+	private $player2;
+
+	public function __construct($player1, $player2) {
+		$this->player1 = $player1;
+		$this->player2 = $player2;
+	}
+
+	public function hasPlayers() {
+		return (isset($this->player1) && isset($this->player2));
+	}
 }
