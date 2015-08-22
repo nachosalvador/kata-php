26e37cfb5e766150422b670d7de1e297c63c3719
Nacho Salvador <nachosalvador83@gmail.com>
2015-08-22 13:13:50 +0200

    Added test to check each player can have concret tennis game points. Added setUp() and tearDown()

--- src/TennisGame/TennisGame.php
+++ src/TennisGame/TennisGame.php
@@ -3,23 +3,63 @@
 namespace TennisGame;
 
 class TennisGame {
-	private $player1;
-	private $player2;
+  const POINTS = array(0, 15, 30, 40);
 
-	public function __construct($player1, $player2) {
-		$this->player1 = $player1;
-		$this->player2 = $player2;
-	}
+  private $player1;
+  private $player2;
+  private $player1_score;
+  private $player2_score;
 
-	public function getPlayer1() {
-		return $this->player1;
-	}
+  public function __construct($player1, $player2) {
+    $this->player1 = $player1;
+    $this->player2 = $player2;
+    $this->player1_score = 0;
+    $this->player2_score = 0;
+  }
 
-	public function getPlayer2() {
-		return $this->player2;
-	}
+  public function getPlayer1() {
+    return $this->player1;
+  }
 
-	public function hasPlayers() {
-		return (isset($this->player1) && isset($this->player2));
-	}
+  public function getPlayer2() {
+    return $this->player2;
+  }
+
+  public function hasPlayers() {
+    return (isset($this->player1) && isset($this->player2));
+  }
+
+  public function getPlayer1Score() {
+    return $this->player1_score;
+  }
+
+  public function getPlayer2Score() {
+    return $this->player2_score;
+  }
+
+  public function getPlayer1Points() {
+    return self::POINTS[$this->player1_score];
+  }
+
+  public function getPlayer2Points() {
+    return self::POINTS[$this->player2_score];
+  }
+
+  public function player1WinPoint() {
+    $score = $this->getPlayer1Score() + 1;
+    $this->setPlayer1Score($score);
+  }
+
+  public function player2WinPoint() {
+    $score = $this->getPlayer2Score() + 1;
+    $this->setPlayer2Score($score);
+  }
+
+  private function setPlayer1Score($score) {
+    $this->player1_score = $score;
+  }
+
+  private function setPlayer2Score($score) {
+    $this->player2_score = $score;  
+  }
 }
--- src/TennisGame/tests/TennisGameTest.php
+++ src/TennisGame/tests/TennisGameTest.php
@@ -5,9 +5,34 @@ namespace TennisGame\Tests;
 use TennisGame\TennisGame;
 
 class testTennisGame extends \PHPUnit_Framework_TestCase {
-	public function testHasPlayers() {
-		$game = new TennisGame("player1", "player2");
-		$this->assertTrue($game->hasPlayers(), "The game has players");
-		$this->assertInternalType("string", $game->getPlayer1(), "The player is a string name");
-	}
+  public $game;
+
+  public function setUp() {
+    $this->game = new TennisGame('player1', 'player2');
+  }
+
+  public function tearDown() {
+    unset($this->game);
+  }
+
+  public function testHasPlayers() {
+    $this->assertTrue($this->game->hasPlayers(), 'The game has players');
+    $this->assertInternalType('string', $this->game->getPlayer1(), 'The player is a string name');
+  }
+
+  public function testEachPlayerCanHaveThePointsZeroFifteenThirtyForty() {
+    $points = array(
+      'zero' => 0,
+      'fifteen' => 15, 
+      'thirty' => 30,
+      'fourty' => 40
+    );
+
+    foreach ($points as $point_name => $point) {
+      $this->assertEquals($point, $this->game->getPlayer1Points(), 'Player1 can have ' . $point_name . ' points');
+      $this->game->player1WinPoint();
+      $this->assertEquals($point, $this->game->getPlayer2Points(), 'Player2 can have ' . $point_name . ' points');
+      $this->game->player2WinPoint();
+    }
+  }
 }
