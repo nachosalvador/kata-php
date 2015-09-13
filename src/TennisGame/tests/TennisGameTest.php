46a10a28bf951897134c08583924f8530e3cd2dc
Nacho Salvador <nachosalvador83@gmail.com>
2015-09-13 19:39:17 +0200

    Refactored some tests and removed unneccesary test function

--- src/TennisGame/tests/TennisGameTest.php
+++ src/TennisGame/tests/TennisGameTest.php
@@ -32,89 +32,15 @@ class testTennisGame extends \PHPUnit_Framework_TestCase {
   public function setPlayersSameScore($score) {
     $this->game->setPlayerScore('player1', $score);
     $this->game->setPlayerScore('player2', $score);
-  }
-
-  public function testEachPlayerCanHaveThePointsZeroFifteenThirtyForty() {
-    foreach (self::POINTS as $point_name => $point) {
-      foreach (array('player1', 'player2') as $player) {
-        $this->assertEquals($point, $this->game->getPlayerPoints($player), ucfirst($player) . ' can have ' . $point_name . ' points');
-        $this->game->playerWinPoint($player);
-      }
-    }
-
-    $fake_points = array(
-      'one' => 1,
-      'three' => 3,
-      'six' => 6,
-      'ten' => 10,
-      'sixteen' => 16, 
-      'thirtytwo' => 32,
-      'fourtyone' => 41
-    );
-    foreach ($fake_points as $point_name => $point) {
-      foreach (array('player1', 'player2') as $player) {
-        $this->assertNotEquals($point, $this->game->getPlayerPoints($player), ucfirst($player) . ' can not have ' . $point_name . ' points');
-        $this->game->playerWinPoint($player);
-      }
-    }
-  }
-
-  public function testPlayerWithFortyPointsAndWinsAPointWinsTheGame() {
-    $this->game->setPlayerScore('player1', self::SCORE['fourty']);
-    $this->game->playerWinPoint('player1');
-    $this->assertTrue($this->game->isPlayerWinGame('player1'), 'The player1 wins the game');
-
-    $this->game->setPlayerScore('player1', self::SCORE['fifteen']);
-    $this->game->playerWinPoint('player1');
-    $this->assertFalse($this->game->isPlayerWinGame('player1'), 'The player1 does not win the game');
-  }
-
-  public function testBothPlayersHaveFortyPointsAreDeuce() {
-    $this->setPlayersSameScore(self::SCORE['fourty']);
-    $this->assertTrue($this->game->arePlayersDeuce(), 'Both players are deuce');
-    
-    $this->setPlayersSameScore(self::SCORE['thirty']);
-    $this->assertFalse($this->game->arePlayersDeuce(), 'Both players are not deuce');
-  }
-
-  public function testBothPlayersAreDeuceTheWinnerOfABallHaveAdvantageAndGameBall() {
-    $this->setPlayersSameScore(self::SCORE['fourty']);
-    $this->game->playerWinPoint('player1');
-    $this->assertTrue($this->game->hasPlayerAdvantageAndGameBall('player1'), 'Player1 has got advantage');
-
-    $this->setPlayersSameScore(self::SCORE['fourty']);
-    $this->game->playerWinPoint('player2');
-    $this->assertFalse($this->game->hasPlayerAdvantageAndGameBall('player1'), 'Player1 has not got advantage');
-  }
-
-  public function testPlayersWithAdvantageWinsAPointWinsTheGame() {
-    $this->setPlayersSameScore(self::SCORE['fourty']);
-    $this->game->playerWinPoint('player1');
-    $this->game->playerWinPoint('player1');
-    $this->assertTrue($this->game->isPlayerWinGame('player1'), 'The player1 wins the game');
-
-    $this->setPlayersSameScore(self::SCORE['fourty']);
-    $this->game->playerWinPoint('player1');
-    $this->game->playerWinPoint('player2');
-    $this->assertFalse($this->game->isPlayerWinGame('player1'), 'The player1 does not win the game');
-  }
-
-  public function testPlayerWithoutAdvantageWinsPointBothPlayersAreDeuce() {
-    $this->setPlayersSameScore(self::SCORE['fourty']);
-    $this->game->playerWinPoint('player1');
-    $this->game->playerWinPoint('player2');
-    $this->assertTrue($this->game->arePlayersDeuce(), 'Both players are deuce');  
-  }
+  } 
 
   public function testGameIsWonByFirstPlayerToHaveAtLeastFourPointsInTotalAndAtLeastTwoPointMoreTThanOpponent() {
-    for ($i = 1; $i <= 4; $i++) {
-      $this->game->playerWinPoint('player1');
-    }
+    $this->game->setPlayerScore('player1', 4);
+    $this->game->setPlayerScore('player2', 2);
     $this->assertTrue($this->game->isPlayerWinGame('player1'), 'The player1 wins the game');
 
-    for ($i = 1; $i <= 3; $i++) {
-      $this->game->playerWinPoint('player2');
-    }
+    $this->game->setPlayerScore('player1', 4);
+    $this->game->setPlayerScore('player2', 3);
     $this->assertFalse($this->game->isPlayerWinGame('player1'), 'The player1 does not win the game');
   }
 
@@ -130,17 +56,10 @@ class testTennisGame extends \PHPUnit_Framework_TestCase {
   }
 
   public function testEachPlayerHasThreeOrMorePointsAndPlayersScoreAreEqualsSoGameScoreIsDeuce() {
-    for ($i = 1; $i <= 8; $i++) {
-      $this->game->playerWinPoint('player1');
-      $this->game->playerWinPoint('player2');
-    }
+    $this->setPlayersSameScore(6);
     $this->assertTrue($this->game->arePlayersDeuce(), 'Both players are deuce');
 
-    $this->setPlayersSameScore(0);
-    for ($i = 1; $i <= 2; $i++) {
-      $this->game->playerWinPoint('player1');
-      $this->game->playerWinPoint('player2');
-    }
+    $this->setPlayersSameScore(2);
     $this->assertFalse($this->game->arePlayersDeuce(), 'Both players are not deuce');
   }
 
