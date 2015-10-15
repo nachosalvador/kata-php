fb03023acdb3b283cc616960aa1523fdc4307841
Nacho Salvador <nachosalvador83@gmail.com>
2015-10-15 12:57:57 +0200

    Added new test. Fixed indetation to two white spaces.

--- src/TradingCardGame/Player.php
+++ src/TradingCardGame/Player.php
@@ -3,19 +3,24 @@
 namespace TradingCardGame;
 
 class Player {
-	private $health;
-	private $mana;
+  private $health;
+  private $mana;
+  private $deck_with_mana_values = array(0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8);
 
-	public function __construct() {
-		$this->health = 30;
-		$this->mana = 0;
-	}
+  public function __construct() {
+    $this->health = 30;
+    $this->mana = 0;
+  }
 
-	public function getHealth() {
-		return $this->health;
-	}
+  public function getHealth() {
+    return $this->health;
+  }
 
-	public function getMana() {
-		return $this->mana;
-	}
+  public function getMana() {
+    return $this->mana;
+  }
+
+  public function getNumberOfCardsInDeck() {
+    return count($this->deck_with_mana_values);
+  }
 }
--- src/TradingCardGame/tests/PlayerTest.php
+++ src/TradingCardGame/tests/PlayerTest.php
@@ -5,10 +5,31 @@ namespace TradingCardGame\Tests;
 use TradingCardGame\Player;
 
 class testPlayer extends \PHPUnit_Framework_TestCase {
-    public function testEachPlayerStartsAGameWithThirtyOfHealthAndZeroOfMana() {
-        $player1 = new Player();
-        $player2 = new Player();
-        $this->assertEquals($player1->getHealth(), 30);
-        $this->assertEquals($player2->getMana(), 0);
-    } 
+  const START_HEATH = 30;
+  const START_MANA = 0;
+
+  public $player1;
+  public $player2;
+
+  public function setUp() {
+    $this->player1 = new Player();
+    $this->player2 = new Player();   
+  }
+
+  public function tearDown() {
+    unset($this->player1);
+    unset($this->player2);
+  }
+
+  public function testEachPlayerStartsAGameWithThirtyOfHealthAndZeroOfMana() {
+    $this->assertEquals($this->player1->getHealth(), self::START_HEATH);
+    $this->assertEquals($this->player2->getMana(), self::START_MANA);
+  } 
+
+  public function testEachPlayerStartsWithATwentyCardsDeck() {
+    $value_mana_cards = array(0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8);
+
+    $this->assertEquals($this->player1->getNumberOfCardsInDeck(), count($value_mana_cards));
+    $this->assertEquals($this->player2->getNumberOfCardsInDeck(), count($value_mana_cards));
+  }
 }
