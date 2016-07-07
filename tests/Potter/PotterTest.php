f8ed955c071ffc8d66f628a2e11307e389757482
Nacho Salvador <nachosalvador83@gmail.com>
2016-07-07 20:18:48 +0200

    Converted to better format.

--- tests/Potter/PotterTest.php
+++ tests/Potter/PotterTest.php
@@ -10,32 +10,52 @@ class testCell extends \PHPUnit_Framework_TestCase {
   const MIN_BOOK_ID = 1;
   const MAX_BOOK_ID = 5;
 
-  public $order;
+  public $basket;
 
   public function setUp() {
-    $this->order = new Basket();
+    $this->basket = new Basket();
   }
 
   public function testOneCopyOfAnyBookCostsEight() {
     for ($i = self::MIN_BOOK_ID; $i <= self::MAX_BOOK_ID; $i++) {
-      $this->order->reset();
-      $this->order->add(new Book($i));
-      $this->assertEquals($this->order->getAmount(), Book::PRICE, 'One copy of book ' . $i . ' of the five books costs 8 EUR.');
+      $this->basket->reset();
+      $this->basket->add(new Book($i));
+      $this->assertEquals(
+        $this->basket->getAmount(),
+        8,
+        'One copy of book ' . $i . ' of the five books costs 8 EUR.'
+      );
     }
   }
 
   public function testSimpleDiscounts() {
-    for ($i = 2; $i <= 5; $i++) {
-      $this->order->reset();
+    $this->basket->reset();
+    $this->basket->add(new Book(1));
+    $this->basket->add(new Book(2));
+    $this->assertEquals(
+      $this->basket->getAmount(),
+      8 * 2 * 0.95,
+      'One copy of 2 different books costs 8 * 2 * 0.95 EUR.'
+    );
 
-      for ($j = 1; $j <= $i; $j++) {
-        $this->order->add(new Book(1));
-      }
-      $this->assertEquals(
-        $this->order->getAmount(),
-        Book::PRICE * $i * Basket::DISCOUNTS[$i],
-        'One copy of ' . $i . ' different books costs ' . Book::PRICE . ' * ' . $i . ' * ' . Basket::DISCOUNTS[$i] . ' EUR.'
-      );
-    }
+    $this->basket->add(new Book(3));
+    $this->assertEquals(
+      $this->basket->getAmount(),
+      8 * 3 * 0.9,
+      'One copy of 3 different books costs 8 * 3 * 0.9 EUR.'
+    );
+    $this->basket->add(new Book(4));
+    $this->assertEquals(
+      $this->basket->getAmount(),
+      8 * 4 * 0.8,
+      'One copy of 4 different books costs 8 * 4 * 0.8 EUR.'
+    );
+
+    $this->basket->add(new Book(5));
+    $this->assertEquals(
+      $this->basket->getAmount(),
+      8 * 5 * 0.75,
+      'One copy of 5 different books costs 8 * 5 * 0.75 EUR.'
+    );
   }
 }
